dfd553bafd835c4a3fb75ad2c3b14cbf24680173
Oscar <voscarmv@gmail.com>
2021-06-17 15:33:54 -0500

    Returned resource

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,8 @@ class SessionsController < Devise::SessionsController
   respond_to :json
   private
   def respond_with(resource, _opts = {})
-    render json: {
-    status: {code: 200, message: 'Logged in successfully.'},
-    data: 'agusuiegusu'
-    }
-  end 
+    render json: resource
+ end 
   def respond_to_on_destroy
     head :no_content
   end
