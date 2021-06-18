a28a8a7c499c3991e6a0008e22438dee263ff0ae
Oscar <voscarmv@gmail.com>
2021-06-18 17:59:30 -0500

    Added response messages to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   resources :appointments
-  devise_for :users, skip: %i[registrations sessions passwords]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    post '/signup', to: 'registrations#create'
-    post '/login', to: 'sessions#create'
-    post '/logout', to: 'sessions#destroy'
-  end
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
+  # devise_for :users, skip: %i[registrations sessions passwords]
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_scope :user do
+  #   post '/signup', to: 'registrations#create'
+  #   post '/login', to: 'sessions#create'
+  #   delete '/logout', to: 'sessions#destroy'
+  # end
 end
