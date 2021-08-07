9f25a155916c903679577b5fccfbe3bde4504ddb
Oscar <voscarmv@gmail.com>
2021-08-06 22:31:16 -0500

    Added netlify site for deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'https://60f6062426d78d008ee480ef--pensive-leavitt-100e0b.netlify.app'
 
     resource '*',
       expose: ['authorization', 'access-control-expose-headers', 'access-token', 'expiry', 'token-type', 'uid', 'client'],
