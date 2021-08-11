29f77858b3950890b9ac93321a736109215014f8
Oscar <voscarmv@gmail.com>
2021-08-10 21:37:27 -0500

    Syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::API
           jwt_payload = JWT.decode(token, Rails.application.credentials.jwt_secret, true, algorithm: 'HS256')          
           user_id = jwt_payload[0]['user_id']
           @current_user_id = user_id
-        rescue JWT::ExpiredSignature, JWT::VerificationError, rescue JWT::DecodeError
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
           head :unauthorized
         end
     else
