8c6c127f45ac882475f633d811c24c422474b426
Oscar Mier <voscarmv@gmail.com>
2021-08-11 14:59:11 -0500

    Update appointments_controller.rb

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,11 +8,6 @@ class AppointmentsController < ApplicationController
     render json: @appointments, include: :subject
   end
 
-  # GET /appointments/1
-  def show
-    render json: @appointment
-  end
-
   # POST /appointments
   def create
     @appointment = Appointment.new(appointment_params)
@@ -22,20 +17,6 @@ class AppointmentsController < ApplicationController
     else
       render json: @appointment.errors, status: :unprocessable_entity
     end
-  end
-
-  # PATCH/PUT /appointments/1
-  def update
-    if @appointment.update(appointment_params)
-      render json: @appointment
-    else
-      render json: @appointment.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /appointments/1
-  def destroy
-    @appointment.destroy
   end
 
   private
