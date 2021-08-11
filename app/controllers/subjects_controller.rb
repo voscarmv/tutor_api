3a908bb500f6af0fa2afde910aaaad881bc7ae80
Oscar Mier <voscarmv@gmail.com>
2021-08-11 15:00:23 -0500

    Update subjects_controller.rb

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,36 +9,6 @@ class SubjectsController < ApplicationController
     render json: @subjects
   end
 
-  # GET /subjects/1
-  def show
-    render json: @subject
-  end
-
-  # POST /subjects
-  def create
-    @subject = Subject.new(subject_params)
-
-    if @subject.save
-      render json: @subject, status: :created, location: @subject
-    else
-      render json: @subject.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /subjects/1
-  def update
-    if @subject.update(subject_params)
-      render json: @subject
-    else
-      render json: @subject.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /subjects/1
-  def destroy
-    @subject.destroy
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
