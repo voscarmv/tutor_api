91b843ab81080ca4c315d34a4f4c19aa1829c2d8
Oscar <voscarmv@gmail.com>
2021-08-11 15:28:23 -0500

    Removed slash from cors URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://inspiring-darwin-0afe5b.netlify.app/'
+    origins 'https://inspiring-darwin-0afe5b.netlify.app'
 
     resource '*',
       expose: ['authorization', 'access-control-expose-headers', 'access-token', 'expiry', 'token-type', 'uid', 'client'],
